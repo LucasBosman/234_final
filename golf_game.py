fd1f0cf735c539cebda7f00863ad8dfd0f85c6f6
LucasBosman <72069030+LucasBosman@users.noreply.github.com>
2024-06-04 14:02:18 -0700

    Add files via upload

---
+++ golf_game.py
@@ -0,0 +1,4 @@
+class GolfGameEnv(gym.Env):
+    def __init__(self, player):
+        super(GolfGameEnv, self).__init__()
+        
---
+++ gym.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+from golf_game import GolfGameEnv
+
+#need to register environment first
+gym.register(
+    id='GolfGame-v0',
+    entr_point='golf_game',
+    kwargs={'player': None}
+)
+
+#create player or sample player from previously made players
+player = 
+
+#Test env
+env = gym.make('GolfGame-v0')
+
+
