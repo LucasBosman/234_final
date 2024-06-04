a1962cd48411dad7d2cced06253e89100935190f
Lucas Bosman <lucasbosman@DN51us9j.SUNet>
2024-06-04 14:56:02 -0700

    first pass

--- golf_game.py
+++ golf_game.py
@@ -1,4 +1,21 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+
 class GolfGameEnv(gym.Env):
-    def __init__(self, player):
+    def __init__(self, player, course):
         super(GolfGameEnv, self).__init__()
-        
+
+
+    def step(self):
+        #update field based on selected action
+
+        pass
+
+    def render(self):
+        #render the game
+
+        pass
+
+    def reset(self):
+        pass
--- gym.py
+++ gym.py
@@ -4,7 +4,7 @@ from golf_game import GolfGameEnv
 #need to register environment first
 gym.register(
     id='GolfGame-v0',
-    entr_point='golf_game',
+    entr_point='golf_game:GolfGameEnv',
     kwargs={'player': None}
 )
 
@@ -13,5 +13,6 @@ player =
 
 #Test env
 env = gym.make('GolfGame-v0')
-
+obs = env.reset()
+env.render()
 
